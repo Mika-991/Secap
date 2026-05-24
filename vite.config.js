e5eb738beebcffcf7f9ee0efee3c63dfbb054f37
Mika-991 <realhamza25@gmail.com>
2026-05-24 16:34:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // If deploying to a GitHub Pages subfolder, set base to your repo name:
-  // base: '/your-repo-name/',
+  base: '/YOUR-REPO-NAME/',
 })
